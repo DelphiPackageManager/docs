6a680a8e19df8af88748dba4835a3d07466347fe
Vincent Parrett <vincent@finalbuilder.com>
2023-08-09 09:39:56 +1000

    added favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "DPM Documentation",
   description: "Documentation",
+  head: [["link", { rel: "icon", href: "/favicon.png" }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
